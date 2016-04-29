22890956de7e9b2003d8a7c8b86b9943be871d86
Koleok <kchamb3@gmail.com>
2016-04-29 00:34:42 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,155 @@
+import {
+  always,
+  anyPass,
+  compose,
+  curry,
+  filter,
+  identity,
+  isEmpty,
+  isNil,
+  keys,
+  last,
+  memoize,
+  not,
+  nthArg,
+  objOf,
+  path as rPath,
+  pathOr,
+  pick,
+  propEq,
+  propOr,
+  reduce,
+  reject,
+} from 'ramda';
+
+/**
+ * return functions that always return their given values
+ */
+export const emptyString = always('');
+export const emptyObject = always({});
+export const emptyArray = always([]);
+
+/**
+ * return functions that return either their first argument or the specified default
+ */
+export const getPropOrEmptyObjectFunction = propOr(emptyObject);
+export const getPropOrEmptyString = propOr('');
+
+/**
+ * Returns the first argument it is invoked with
+ *
+ * @param  {Args} `args` arguments at the beginning of compose pipe
+ * @return {*}
+ */
+export const firstArgument = nthArg(0);
+
+/**
+ * Returns the second argument it is invoked with
+ *
+ * @param  {Args} `args` arguments at the beginning of compose pipe
+ * @return {*}
+ */
+export const secondArgument = nthArg(1);
+
+/**
+ * Check whether a value of any type is Empty, Null, or undefined
+ *
+ * @param  {*} `val`  val to pipe through predicates
+ * @return {Boolean}
+ */
+export const isNilOrEmpty = anyPass([isNil, isEmpty]);
+
+/**
+ * Check if a key exists at a deep path
+ *
+ * @param  {Array} `arr`  list of strings used as path to prop
+ * @return {Boolean}
+ */
+export const hasDeep = curry(
+  compose(not, isNil, pathOr(null)),
+);
+
+/**
+ * Return a whitelisted set of keys from nested object path
+ *
+ * @param  {Array} `arr`  list of strings used as path to prop
+ * @return {Boolean}
+ */
+export const pickDeep = memoize(curry(
+  (pathToProp, pickList, data) => {
+    const processor = compose(
+      objOf(last(pathToProp)),
+      isEmpty(pickList) ? identity : pick(pickList),
+      rPath(pathToProp),
+    );
+
+    return processor(data);
+  })
+);
+
+/**
+ * Higher order function to apply a property matching predicate to list
+ * transformation functions like filter or reject
+ *
+ * @param  {Function} func  function to apply prop matching predicate to
+ * @param  {String}   key   `key` key to match with the specified value
+ * @param  {String}   val   `val` value to match on the specified key
+ * @return {Function}
+ */
+const applyByProp = curry(
+  (func, key, val, data) => func(propEq(key, val), data),
+);
+
+/**
+ * Curryable function to filter a list of objects according to the value of
+ * a specified property
+ *
+ * @param  {String} `key` key to match with the specified value
+ * @param  {String} `val` value to match on the specified key
+ * @return {Function}
+ */
+export const filterByProp = applyByProp(filter);
+
+/**
+ * Curryable function to drop items from a list of objects according to the
+ * value of a specified property
+ *
+ * @param  {String} `key` key to match with the specified value
+ * @param  {String} `val` value to match on the specified key
+ * @return {Function}
+ */
+export const dropByProp = applyByProp(reject);
+
+// rejectors for common property names
+export const dropById = dropByProp('id');
+
+/**
+ * Creates a new object with the own properties of the provided object, but the
+ * keys renamed according to the keysMap object as `{oldKey: newKey}`.
+ * When some key is not found in the keysMap, then it's passed as-is.
+ *
+ * @sig {a: b} -> {a: *} -> {b: *}
+ */
+export const renameKeys = curry(
+  (keysMap, obj) => reduce((acc, key) => {
+    acc[keysMap[key] || key] = obj[key];
+
+    return acc;
+  }, {}, keys(obj))
+);
+
+export default {
+  dropById,
+  dropByProp,
+  emptyArray,
+  emptyObject,
+  emptyString,
+  filterByProp,
+  firstArgument,
+  getPropOrEmptyObjectFunction,
+  getPropOrEmptyString,
+  hasDeep,
+  isNilOrEmpty,
+  renameKeys,
+  secondArgument,
+};
---
+++ test/unit-tests.js
@@ -0,0 +1,425 @@
+import { expect } from 'chai';
+import {
+  shouldBeABoolean,
+  shouldBeAFunction,
+  shouldBeAnArray,
+  shouldBeAnObject,
+  shouldBeANumber,
+  shouldBeAString,
+  shouldBeEmpty,
+  shouldNotBeNull,
+  shouldNotBeUndefined,
+  testIfExists,
+  testSet,
+} from 'how-the-test-was-won';
+
+import {
+  dropByProp,
+  emptyArray,
+  emptyObject,
+  emptyString,
+  filterByProp,
+  firstArgument,
+  getPropOrEmptyObjectFunction,
+  getPropOrEmptyString,
+  hasDeep,
+  pickDeep,
+  isNilOrEmpty,
+  renameKeys,
+  secondArgument,
+} from './general-utils';
+
+describe('General Utils', () => {
+  describe('#emptyString', () => {
+    const result = emptyString();
+
+    shouldBeEmpty(result);
+    shouldBeAString(result);
+    shouldNotBeNull(result);
+    shouldNotBeUndefined(result);
+  });
+
+  describe('#emptyObject', () => {
+    describe('when invoked', () => {
+      const result = emptyObject();
+
+      shouldBeEmpty(result);
+      shouldBeAnObject(result);
+      shouldNotBeNull(result);
+      shouldNotBeUndefined(result);
+    });
+  });
+
+  describe('#emptyArray', () => {
+    describe('when invoked', () => {
+      const result = emptyArray();
+
+      shouldBeEmpty(result);
+      shouldBeAnArray(result);
+      shouldNotBeNull(result);
+      shouldNotBeUndefined(result);
+    });
+  });
+
+  describe('#getPropOrEmptyObjectFunction', () => {
+    describe('given a valid non-empty object', () => {
+      const testObj = { one: 1, two: 2, three: 3 };
+
+      describe('when invoked with a key that is present on the object', () => {
+        const result = getPropOrEmptyObjectFunction('one', testObj);
+
+        testIfExists(result);
+        shouldBeANumber(result);
+        it('should return the property', () => {
+          expect(result).to.equal(testObj.one);
+        });
+      });
+
+      describe('when invoked with a key that is not present on the object', () => {
+        const result = getPropOrEmptyObjectFunction('four', testObj)();
+
+        shouldBeAnObject(result);
+        shouldBeEmpty(result);
+      });
+    });
+
+    describe('given an empty object', () => {
+      describe('when invoked with a valid key', () => {
+        const result = getPropOrEmptyObjectFunction('one', {})();
+
+        shouldBeEmpty(result);
+        shouldBeAnObject(result);
+        shouldNotBeNull(result);
+        shouldNotBeUndefined(result);
+      });
+    });
+  });
+
+  describe('#getPropOrEmptyString', () => {
+    describe('given a valid non-empty object', () => {
+      const testObj = { one: 1, two: 2, three: 3 };
+
+      describe('when invoked with a key that is present on the object', () => {
+        const result = getPropOrEmptyString('one', testObj);
+
+        testIfExists(result);
+        shouldBeANumber(result);
+        it('should return the property', () => {
+          expect(result).to.equal(testObj.one);
+        });
+      });
+
+      describe('when invoked with a key that is not present on the object', () => {
+        const result = getPropOrEmptyString('four', testObj);
+
+        shouldBeAString(result);
+        shouldBeEmpty(result);
+        shouldNotBeNull(result);
+        shouldNotBeUndefined(result);
+      });
+    });
+
+    describe('given an empty object', () => {
+      describe('when invoked with a valid key', () => {
+        const result = getPropOrEmptyString('one', {});
+
+        shouldBeEmpty(result);
+        shouldBeAString(result);
+        shouldNotBeNull(result);
+        shouldNotBeUndefined(result);
+      });
+    });
+  });
+
+  describe('#firstArgument', () => {
+    describe('when invoked with three arguments', () => {
+      const result = firstArgument('one', 'two', 'three');
+
+      testIfExists(result);
+      shouldBeAString(result);
+      it('should return the first argument', () => {
+        expect(result).to.equal('one');
+      });
+    });
+  });
+
+  describe('#secondArgument', () => {
+    describe('when invoked with three arguments', () => {
+      const result = secondArgument('one', 'two', 'three');
+
+      testIfExists(result);
+      shouldBeAString(result);
+      it('should return the second argument', () => {
+        expect(result).to.equal('two');
+      });
+    });
+  });
+
+  describe('#isNilOrEmpty', () => {
+    const set = [
+      ['given a null value', null, true],
+      ['given a undefined value', undefined, true],
+      ['given an empty string', '', true],
+      ['given an empty object', {}, true],
+      ['given an empty array', [], true],
+      ['given a non empty string', 'test', false],
+      ['given a non empty array', ['test'], false],
+      ['given a non empty object', { test: 'test' }, false],
+      ['given a NaN value', NaN, false],
+    ];
+
+    set.forEach(test => testSet(test, isNilOrEmpty));
+  });
+
+  describe('#hasDeep', () => {
+    const testObj = { one: { two: { three: 'im in here' } } };
+
+    describe('given an object with a nested key', () => {
+      describe('when passed a valid path', () => {
+        const result = hasDeep(['one', 'two', 'three'], testObj);
+
+        testIfExists(result);
+        shouldBeABoolean(result);
+        it('should return true', () => {
+          expect(result).to.equal(true);
+        });
+      });
+
+      describe('when passed a invalid path', () => {
+        const result = hasDeep(['one', 'two', 'four'], testObj);
+
+        shouldBeABoolean(result);
+        shouldNotBeNull(result);
+        shouldNotBeUndefined(result);
+        it('should return false', () => {
+          expect(result).to.equal(false);
+        });
+      });
+
+      describe('when passed an empty array as path', () => {
+        const result = hasDeep([], testObj);
+
+        shouldBeABoolean(result);
+        shouldNotBeNull(result);
+        shouldNotBeUndefined(result);
+        it('should return true', () => {
+          expect(result).to.equal(true);
+        });
+      });
+    });
+  });
+
+  describe('#pickDeep', () => {
+    const testObj = {
+      one: 1,
+      two: 2,
+      deepObj: {
+        deeperObj: { three: 3, four: 4, five: 5 },
+      },
+    };
+
+    const testPrim = {
+      one: 1,
+      two: 2,
+      deepObj: {
+        deeperObj: true,
+      },
+    };
+
+    const checkAgainstExpected = result => {
+      it('should return the expected result', () => {
+        expect(result).to.deep.equal({ deeperObj: { three: 3, four: 4 } });
+      });
+    };
+
+    describe('when passsed a valid path and picklist and partially applied', () => {
+      const picker = pickDeep(
+        ['deepObj', 'deeperObj'],
+        ['three', 'four'],
+      );
+
+      testIfExists(picker);
+      shouldBeAFunction(picker);
+
+      describe('when the resulting function is passed a valid object', () => {
+        const result = picker(testObj);
+
+        testIfExists(result);
+        shouldBeAnObject(result);
+        checkAgainstExpected(result);
+      });
+    });
+
+    describe('when passsed a valid path and picklist and fully applied', () => {
+      const result = pickDeep(
+        ['deepObj', 'deeperObj'],
+        ['three', 'four'],
+        testObj,
+      );
+
+      testIfExists(result);
+      shouldBeAnObject(result);
+      checkAgainstExpected(result);
+    });
+
+    describe('when passsed a valid path and empty picklist and fully applied', () => {
+      describe('when the value at specified path is an object', () => {
+        const result = pickDeep(
+          ['deepObj', 'deeperObj'], [], testObj,
+        );
+
+        testIfExists(result);
+        shouldBeAnObject(result);
+        it('should return an unaltered version of the nested object', () => {
+          expect(result).to.deep.equal(testObj.deepObj);
+        });
+      });
+
+      describe('when the value at specified path is a boolean', () => {
+        const result = pickDeep(
+          ['deepObj', 'deeperObj'], [], testPrim,
+        );
+
+        testIfExists(result);
+        shouldBeAnObject(result);
+        it('should return an unaltered version of the nested object', () => {
+          expect(result).to.deep.equal(testPrim.deepObj);
+        });
+      });
+    });
+
+    describe('when passsed a valid path and empty picklist and fully applied', () => {
+      const result = pickDeep(
+        ['deepObj', 'deeperObj'], [], testObj,
+      );
+
+      testIfExists(result);
+      shouldBeAnObject(result);
+      it('should return an unaltered version of the nested object', () => {
+        expect(result).to.deep.equal(testObj.deepObj);
+      });
+    });
+  });
+
+  describe('#filterByProp', () => {
+    describe('when passed a key and value argument and partially applied', () => {
+      const target = 'i am the one you seek';
+      const testFilter = filterByProp('test', target);
+
+      testIfExists(testFilter);
+      shouldBeAFunction(testFilter);
+
+      describe('when the resulting function is passed a list of objects with a "test" key', () => {
+        const testArr = [
+          { test: 1 },
+          { test: 2 },
+          { test: 3 },
+          { test: target },
+        ];
+
+        const result = testFilter(testArr);
+
+        testIfExists(result);
+        shouldBeAnArray(result);
+
+        it('should have one element', () => {
+          expect(result).to.have.lengthOf(1);
+        });
+
+        it('first element should be an object', () => {
+          expect(result[0]).to.be.an('object');
+        });
+
+        it('should equal the expected element of the provided object array', () => {
+          expect(result[0]).to.deep.equal(testArr[3]);
+        });
+      });
+    });
+
+    describe('when passed a key, value, and data array and fully applied', () => {
+      const target = 'i am the one you seek';
+      const testArr = [
+        { test: 1 },
+        { test: 2 },
+        { test: 3 },
+        { test: target },
+      ];
+
+      const result = filterByProp('test', target, testArr);
+
+      testIfExists(result);
+      shouldBeAnArray(result);
+
+      it('should have one element', () => {
+        expect(result).to.have.lengthOf(1);
+      });
+
+      it('first element should be an object', () => {
+        expect(result[0]).to.be.an('object');
+      });
+
+      it('should equal the expected element of the provided object array', () => {
+        expect(result[0]).to.deep.equal(testArr[3]);
+      });
+    });
+  });
+
+  describe('#dropByProp', () => {
+    describe('when passed a key and value argument', () => {
+      const target = 'i am the one you seek';
+      const testDrop = dropByProp('test', target);
+
+      testIfExists(testDrop);
+      shouldBeAFunction(testDrop);
+
+      describe('when the resulting function is passed a list of objects with a "test" key', () => {
+        const testArr = [
+          { test: 1 },
+          { test: 2 },
+          { test: 3 },
+          { test: target },
+        ];
+        const expected = [
+          { test: 1 },
+          { test: 2 },
+          { test: 3 },
+        ];
+
+        const result = testDrop(testArr);
+
+        testIfExists(result);
+        shouldBeAnArray(result);
+
+        it('should have 3 elements', () => {
+          expect(result).to.have.lengthOf(3);
+        });
+
+        it('first element should be an object', () => {
+          expect(result[0]).to.be.an('object');
+        });
+
+        it('should equal the expected element of the provided object array', () => {
+          expect(result).to.deep.equal(expected);
+        });
+      });
+    });
+  });
+
+  describe('#renameKeys', () => {
+    const testObj = { one: 1, two: 2, three: 3 };
+    const renameMap = { three: 'wigglesaurus' };
+
+    const result = renameKeys(renameMap, testObj);
+
+    testIfExists(result);
+    shouldBeAnObject(result);
+
+    it('should contain the new key', () => {
+      expect(result).to.contain.keys({ wigglesaurus: 3 });
+    });
+
+    it('should remove the mapped key', () => {
+      expect(result).to.not.contain.keys(['three']);
+    });
+  });
+});
